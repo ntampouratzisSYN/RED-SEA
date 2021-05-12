2e3198077e7f690c9cf9480c9fd3b415bea7a0a2
ntampouratzis <32739524+ntampouratzis@users.noreply.github.com>
2021-05-12 12:17:06 +0300

    Update cossim_install.sh

--- cossim_install.sh
+++ cossim_install.sh
@@ -1,4 +1,5 @@
 #Setup on Ubuntu 18.04
+sudo apt update
 
 #gem5 prerequisites
 sudo apt install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python3-dev python3-six python libboost-all-dev pkg-config
@@ -7,4 +8,4 @@ sudo apt install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev 
 sudo apt install cmake bison flex libxml2-dev
 
 #omnet 5.0 prerequisites
-sudo apt install tcl-dev tk-dev qt4-qmake libqt4-dev libqt4-opengl-dev openmpi-bin libopenmpi-dev clang
+sudo apt install openjdk-8-jdk openjdk-8-jre tcl-dev tk-dev qt4-qmake libqt4-dev libqt4-opengl-dev openmpi-bin libopenmpi-dev clang
