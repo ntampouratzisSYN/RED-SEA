246086a98402f509ea79debca08bb305e4689096
ntampouratzis <32739524+ntampouratzis@users.noreply.github.com>
2021-05-12 12:13:34 +0300

    Create cossim_install.sh

---
+++ cossim_install.sh
@@ -0,0 +1,8 @@
+#gem5 prerequisites
+sudo apt install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python3-dev python3-six python libboost-all-dev pkg-config
+
+#certi prerequisites
+sudo apt install cmake bison flex libxml2-dev
+
+#omnet 5.0 prerequisites
+sudo apt install tcl-dev tk-dev qt4-qmake libqt4-dev libqt4-opengl-dev openmpi-bin libopenmpi-dev clang
